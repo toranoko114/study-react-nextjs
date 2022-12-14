a79c9ded4c377610a17713b83001148bae581cca
toranoko114 <toranoko114.business@gmail.com>
2022-12-14 14:56:52 +0900

    :sparkles: Header.jsの追加

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+// NextJSの機能 prefetch機能があり
+import Link from 'next/link'
+import styles from '../styles/Header.module.css'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <p className={styles.anchor}>Index</p>
+      </Link>
+      <Link href="/about">
+        <p className={styles.anchor}>About</p>
+      </Link>
+    </header>
+  )
+}
