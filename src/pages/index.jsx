3927a492580ffc296068a3066a1fddd0e8eae938
toranoko114 <toranoko114.business@gmail.com>
2022-12-17 15:41:31 +0900

    ✨ useEffect導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,18 +3,27 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import Link from 'next/link'
 
 export default function Home() {
 
-const foo = 1;
+  const foo = 1;
 
-const handleClick = useCallback((e) => {
-  console.log();
-  e.preventDefault();
-  alert(foo);
+  const handleClick = useCallback((e) => {
+    console.log();
+    e.preventDefault();
+    alert(foo);
   }, []);
+
+  useEffect(() => {
+    console.log('マウント時')
+    document.body.style.backgroundColor = 'lightblue';
+    return () => {
+      console.log('アンマウント時')
+      document.body.style.backgroundColor = '';
+    };
+  },[]);
 
 return (
   <div className={styles.container}>
