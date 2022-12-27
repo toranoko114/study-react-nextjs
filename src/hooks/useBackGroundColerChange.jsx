5144e839e777f820a8436abe67ecdbd55ac20d44
toranoko114 <toranoko114.business@gmail.com>
2022-12-27 09:22:22 +0900

    ✨ NextJSの機能であるuseRouterを学習

--- src/hooks/useBackGroundColerChange.jsx
+++ src/hooks/useBackGroundColerChange.jsx
@@ -1,12 +1,29 @@
-import { useEffect} from 'react'
+import { useRouter } from 'next/router';
+import { useEffect, useMemo} from 'react'
 
 export const useBackGroundColerChange = () => {
 
+  const router = useRouter();
+
+  const backGroundColer = useMemo(()=>{
+    switch(router.pathname){
+      case "/": {
+        return "lightblue";
+      }
+      case "/about": {
+        return "beige";
+      }
+      default: {
+        return ""
+      }
+    }
+  }, [router.pathname])
+
     useEffect(() => {
-      document.body.style.backgroundColor = 'lightblue';
+      document.body.style.backgroundColor = backGroundColer;
       return () => {
         document.body.style.backgroundColor = '';
       };
-    },[]);
+    },[backGroundColer]);
 
   }
